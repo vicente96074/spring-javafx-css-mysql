7da8148abfeef4a661ce498db84dfa0d6a6766ae
Augusto Vicente <vicente96074@gmail.com>
2022-10-25 19:59:33 -0600

    Agregar plugin para empaquetar lib

---
+++ src/main/java/com/softquark/application/Main.java
@@ -0,0 +1,12 @@
+package com.softquark.application;
+
+/**
+ *
+ * @author Augusto Vicente
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
