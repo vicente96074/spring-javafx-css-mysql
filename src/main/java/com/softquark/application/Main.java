edd7bb101fefcce03d84c09fa512faa76c225894
Augusto Vicente <augustovicente201831464@cunoc.edu.gt>
2022-10-27 11:24:19 -0600

    Agregar plugin para empaquetar proyecto en un solo jar

---
+++ src/main/java/com/softquark/application/Main.java
@@ -0,0 +1,13 @@
+package com.softquark.application;
+
+/**
+ * Clase main
+ *
+ * @author Augusto Vicente
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
